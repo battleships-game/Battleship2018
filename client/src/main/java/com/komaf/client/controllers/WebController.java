d443b5ff5537c05808058f10c3f3040110d8688d
Mikolaj Olszowiec <mikolaj_olszowiec@epam.com>
2018-12-12 13:32:40 +0100

    Aby do naszej aplikacji dać dostęp przez przeglądarkę dodałem klasę WebController która zawiera w sobie dwa kontrolery.

---
+++ client/src/main/java/com/komaf/client/controllers/WebController.java
@@ -0,0 +1,20 @@
+package com.komaf.client.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class WebController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String indexController() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/setBoard", method = RequestMethod.GET)
+    public String setBoardController() {
+        return "settingBoard";
+    }
+
+
+}
