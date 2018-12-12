37b59054559f42ed80d0261cdae35ac4d328823e
Mikolaj Olszowiec <mikolaj_olszowiec@epam.com>
2018-12-12 13:43:48 +0100

    W celu zapewnienia większej czytelności kodu części frontendowej dokonałem refaktoryzacji kodu źródłowego.

--- client/src/main/resources/static/board.js
+++ client/src/main/resources/static/board.js
@@ -1,10 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 $(document).ready(function() {
     var space = 1;
     for (var r = 0; r < 10; r++) {
@@ -13,7 +6,7 @@ $(document).ready(function() {
             col += "<td data-pos='" + space + "'><div class='emptyField rounded' dataX='" + c + "' dataY='" + r + "'></div></td>";
             space++;
         }
-        $("#chessboard").append("<tr>" + col + "</tr>");
+        $("#playersBoardTable").append("<tr>" + col + "</tr>");
     }
     var space2 = 1;
     for (var r = 0; r < 10; r++) {
@@ -22,14 +15,13 @@ $(document).ready(function() {
             col2 += "<td data-pos='" + space2 + "'><div class='enemyField rounded' dataX='" + c + "' dataY='" + r + "'></div></td>";
             space2++;
         }
-        $("#chessboard2").append("<tr>" + col2 + "</tr>");
+        $("#enemyBoardTable").append("<tr>" + col2 + "</tr>");
     }
 });
 
 $(document).on('click', "div.enemyField", function() {
     var dataX = $(this).attr("dataX");
     var dataY = $(this).attr("dataY");
-//    alert("Kliknales pole: " + "x= " + dataX + ", y= " + dataY);
     alert("Wysyłamy to pole do serwera-klienta: " + "x= " + dataX + ", y= " + dataY);
 });
 
