11f8afb15a2cc3b4c1b7b2b6ec84cee138e3329c
Augustyn_Sybidlo <augustyn_sybidlo@epam.com>
2018-12-17 11:15:37 +0100

    #T43 dodanie testów do modułu server klasy RoomRepository celem pokrycia testami

--- server/src/main/java/com/komaf/server/repository/RoomRepository.java
+++ server/src/main/java/com/komaf/server/repository/RoomRepository.java
@@ -13,17 +13,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Repository
-public class RoomRepository{
+public class RoomRepository {
 
     private List<Room> roomList;
 
     public RoomRepository() {
         this.roomList = new ArrayList<>();
-        roomList.add(new Room(new Player("jan")));
-        roomList.add(new Room(new Player("janusz")));
-        roomList.add(new Room(new Player("magda")));
-        roomList.add(new Room(new Player("piotr")));
-        roomList.add(new Room(new Player("pioter"), new Player("Andrzej")));
     }
 
     public List<Room> findAll() {
@@ -33,9 +28,11 @@ public class RoomRepository{
     public ResponseEntity<Room> save(Room newRoom) {
         roomList.add(newRoom);
         return new ResponseEntity<>(HttpStatus.CREATED);
-        }
+    }
 
     public List<Room> getFreeRooms() {
-        return roomList.stream().filter(a -> a.getRoomStatus().equals(RoomStatus.FREE)).collect(Collectors.toList());
+        return roomList.stream().filter(room -> room.getRoomStatus()
+                .equals(RoomStatus.FREE))
+                .collect(Collectors.toList());
     }
 }
